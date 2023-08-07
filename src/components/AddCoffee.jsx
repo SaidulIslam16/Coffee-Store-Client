093f79ce955ee01829c6f03d529fbc8985d7ac80
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-07 22:56:58 +0600

    Added coffee successfully and send it to backend

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 const AddCoffee = () => {
     const handleAddCoffee = event => {
         event.preventDefault();
@@ -15,6 +17,27 @@ const AddCoffee = () => {
         const newCoffee = { name, quantity, supplire, test, category, details, photoUrl }
 
         console.log(newCoffee);
+
+        // send data to the server.
+        fetch('http://localhost:5000/coffee', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'User Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Confirm'
+                    })
+                }
+            })
     }
 
     return (
